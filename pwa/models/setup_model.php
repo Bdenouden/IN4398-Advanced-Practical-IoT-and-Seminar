535780f259958d9db71e7be4dccf4626cc0db8cd
Maarten <maarten.de.jong@hotmail.com>
2020-09-18 13:38:51 +0200

    Crude initial setup procedure created
    Forces user to register an initial account, becomes an admin account. Only after this creation will you be able to continue.

--- pwa/models/setup_model.php
+++ pwa/models/setup_model.php
@@ -1,6 +1,42 @@
 <?php
 class setup_model extends Model {
 
+    public function requestAccountConfirmation($username, $password) {
+
+        $result = Database::select("SELECT * FROM users WHERE user_name = :username", array(':username' => $username));
+
+        if(isset($result[0])){
+
+            throw new UserException("An account is already registered with this username!");
+
+        }
+        else{
+
+            Database::beginTransaction();
+
+            Database::query("INSERT INTO users (user_name, user_password, user_type) VALUES (:username, :password, :type)",
+                array(
+                    ':username' => $username,
+                    ':password' => $password,
+                    ':type' => "admin",
+                )
+            );
+
+            try{
+                fclose(fopen("setup.lock", "w"));
+
+                Database::commit();
+
+                Auth::redirect("/login?setup_success=true");
+
+            }
+            catch (Exception $e){
+                var_dump($e);
+                Database::rollBack();
+            }
+
+        }
+    }
 
 }
 
