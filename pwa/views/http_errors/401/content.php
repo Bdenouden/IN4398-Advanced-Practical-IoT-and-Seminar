b64647addba6852c206e9aa387d5269f8b314072
Maarten <maarten.de.jong@hotmail.com>
2020-09-23 17:04:33 +0200

    Send correct headers for http_errors

--- pwa/views/http_errors/401/content.php
+++ pwa/views/http_errors/401/content.php
@@ -1,4 +1,4 @@
-
+<?php header('HTTP/1.0 401 Unauthorized'); ?>
 <section style="padding:90px 0;">
 	<div class="container">
 		<div class="text-center">
--- pwa/views/http_errors/500/content.php
+++ pwa/views/http_errors/500/content.php
@@ -1,4 +1,4 @@
-
+<?php header('HTTP/1.0 500 Internal Server Error'); ?>
 <section style="padding:90px 0;">
 	<div class="container">
 		<div class="text-center">
