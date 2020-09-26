b58a383682f19179a9118ed5ffc137f7459ce813
bram <bramdenouden96@hotmail.com>
2020-09-26 17:28:18 +0200

    sensor node emulator created

---
+++ Server/webSocketClient.py
@@ -0,0 +1,44 @@
+import asyncio
+import websockets
+import json
+
+uri = "ws://localhost:8765"
+data = {
+    "chipID": 1234567,
+    "version": "testClientV1",
+    "battery": 100,
+    "soil_moisture": 50,
+    "air_humidity": 250,
+    "temperature": 125,
+    "pH": 70
+}
+exit_msg = "bye"
+
+
+async def client():
+    global uri, data, exit_msg
+
+    while True:
+        async with websockets.connect(uri) as websocket:
+            print(f"[WSC] Connected to {uri}")
+
+            json_out = json.dumps(data)
+            await websocket.send(json_out)
+            print("[WSC] JSON sent")
+
+            tbr_string = await websocket.recv()
+            print(f"[WSC] incomming message: {tbr_string}")
+            tbr = int(tbr_string.split(':')[-1])
+
+            msg = ''
+            while msg != exit_msg:
+                msg = await websocket.recv()
+                print(f"[WSC] msg in: {msg}")
+            print(
+                f"[WSC] connection closed, reconnecting in {int(tbr/1000)} seconds\n")
+            await asyncio.sleep(int(tbr/1000))
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(client())
+loop.run_forever()
