1b952bd1a82175b2653428ab5b5a72ed9dc1ac70
bram <bramdenouden96@hotmail.com>
2020-09-26 18:11:06 +0200

    added reconnect if server unavailable

--- Server/webSocketClient.py
+++ Server/webSocketClient.py
@@ -19,24 +19,30 @@ async def client():
     global uri, data, exit_msg
 
     while True:
-        async with websockets.connect(uri) as websocket:
-            print(f"[WSC] Connected to {uri}")
+        try:
+            async with websockets.connect(uri) as websocket:
+                print(f"[WSC] Connected to {uri}")
 
-            json_out = json.dumps(data)
-            await websocket.send(json_out)
-            print("[WSC] JSON sent")
+                json_out = json.dumps(data)
+                await websocket.send(json_out)
+                print("[WSC] JSON sent")
+                print(json_out)
 
-            tbr_string = await websocket.recv()
-            print(f"[WSC] incomming message: {tbr_string}")
-            tbr = int(tbr_string.split(':')[-1])
+                tbr_string = await websocket.recv()
+                print(f"[WSC] incomming message: {tbr_string}")
+                tbr = int(tbr_string.split(':')[-1])
 
-            msg = ''
-            while msg != exit_msg:
-                msg = await websocket.recv()
-                print(f"[WSC] msg in: {msg}")
-            print(
-                f"[WSC] connection closed, reconnecting in {int(tbr/1000)} seconds\n")
-            await asyncio.sleep(int(tbr/1000))
+                msg = ''
+                while msg != exit_msg:
+                    msg = await websocket.recv()
+                    print(f"[WSC] msg in: {msg}")
+                print(
+                    f"[WSC] connection closed, reconnecting in {int(tbr/1000)} seconds\n")
+                await asyncio.sleep(int(tbr/1000))
+        except:
+            print(f"[WSC] Could not connect to {uri}, will attempt again in a few seconds")
+            await asyncio.sleep(10)
+
 
 
 loop = asyncio.get_event_loop()
