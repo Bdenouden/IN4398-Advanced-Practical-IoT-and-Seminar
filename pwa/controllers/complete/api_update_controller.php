bd837e1d2275e195b0ed853d145ec6c1a12585b3
Maarten <maarten.de.jong@hotmail.com>
2020-09-28 20:50:12 +0200

    Fix /update measure time API

--- pwa/controllers/complete/api_update_controller.php
+++ pwa/controllers/complete/api_update_controller.php
@@ -29,8 +29,9 @@ class api_update_controller extends Controller
 
                     foreach ($data as $node_chipid => $entry) {
                         if (is_array($entry)) {
+                            $measure_time = isset($entry['measure_time']) ? $entry['measure_time'] : null;
+                            unset($entry['measure_time']);
                             foreach ($entry as $sensor_uid => $sensor_data) {
-                                $measure_time = isset($sensor_data['measure_time']) ? $sensor_data['measure_time'] : null;
                                 if (!$this->model->storeSensorEntry($node_chipid, $sensor_uid, $sensor_data['name'], $sensor_data['value'], $sensor_data['unit'], $measure_time)) {
                                     $failures[] = [$node_chipid, $sensor_uid];
                                 }
