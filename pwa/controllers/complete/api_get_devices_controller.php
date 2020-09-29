1f515817d1172d44925f28f854888b0fd699245b
Maarten <maarten.de.jong@hotmail.com>
2020-09-29 12:45:52 +0200

    Return /get_devices with expected data
    Closes #11

--- pwa/controllers/complete/api_get_devices_controller.php
+++ pwa/controllers/complete/api_get_devices_controller.php
@@ -13,7 +13,34 @@ class api_get_devices_controller extends Controller
             $login = Auth::requireBasicLogin();
 
             if ($login){
-                echo json_encode($this->model->getKnownDevices());
+                $devices = $this->model->getKnownDevices();
+
+                $output = [];
+                foreach($devices as $device){
+                    $sensor_data = [
+                        "id" => $device["id"],
+                        "name" => $device["name"],
+                        "type" => $device["type"],
+                        "rawMinVal" => $device["rawMinVal"],
+                        "rawMaxVal" => $device["rawMaxVal"],
+                        "minVal" => $device["minVal"],
+                        "maxVal" => $device["maxVal"]
+                    ];
+
+                    if (array_key_exists($device["node_id"], $output)){
+                        $output[$device["node_id"]]["sensors"][] = $sensor_data;
+                    }
+                    else {
+                        $output[$device["node_id"]] = [
+                            "id" => $device["id"],
+                            "added" => $device["added"],
+                            "is_active" => $device["is_active"],
+                            "sensors" => [$sensor_data]
+                        ];
+                    }
+                }
+
+                echo json_encode($output);
             }
             else {
                 $this->loadController('e403');
---
+++ pwa/sql/06 link_sensors_to_nodes.sql
@@ -0,0 +1,26 @@
+create table sensor_types
+(
+    id int auto_increment,
+    name varchar(40) not null,
+    type varchar(20) not null,
+    rawMinVal int not null,
+    rawMaxVal int not null,
+    minVal int not null,
+    maxVal int null,
+    constraint sensor_types_pk
+        primary key (id)
+);
+
+create table sensor_node_link
+(
+    id int auto_increment,
+    node_id varchar(20) null,
+    sensor_type_id int null,
+    constraint sensor_node_link_pk
+        primary key (id),
+    constraint sensor_node_link_sensor_nodes_id_fk
+        foreign key (node_id) references sensor_nodes (id),
+    constraint sensor_node_link_sensor_types_id_fk
+        foreign key (sensor_type_id) references sensor_types (id)
+);
+
