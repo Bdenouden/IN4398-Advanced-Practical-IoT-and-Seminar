bcafa3c47ecb3a1eb3424aa1760b66220931724a
Maarten <maarten.de.jong@hotmail.com>
2020-10-21 10:52:46 +0200

    Adjust python code for PEP-8 compliancy

--- Server/objects/node.py
+++ Server/objects/node.py
@@ -9,26 +9,26 @@ class Node:
     __isActive = False
     isNew = False
 
-    def __init__(self, chipId, version, config_version, sensorList=None):
-        self.chipId = chipId
+    def __init__(self, chip_id, version, config_version, sensor_list=None):
+        self.chipId = chip_id
         self.version = version
         self.config_version = config_version
         self.created_at = datetime.now().strftime("%Y.%m.%d - %H:%M:%S")
         self.updated_at = datetime.now().strftime("%Y.%m.%d - %H:%M:%S")
-        if sensorList is None:
+        if sensor_list is None:
             self.sensorList = []
         else:
-            self.sensorList = sensorList
+            self.sensorList = sensor_list
         Node.knownDevices[self.chipId] = self
 
-        print(f"[NODE] ChipId: {self.chipId}, Sensorlist: ",end='')
-        print(sensorList)
+        print(f"[NODE] ChipId: {self.chipId}, Sensorlist: ", end='')
+        print(sensor_list)
 
-    def sensorDataFromJson(self, json):
+    def sensor_data_from_json(self, json):
         # print(f"[NODE] sensordataformjson json = {json}")
         self.config_version = json.get('config_version')
         for sensor in self.sensorList:
-            sensor.setValue(json.get(sensor.link_id)) #FIXME
+            sensor.set_value(json.get(sensor.link_id))  # FIXME
             print(sensor)
             print(f"[NODE] [sensorDataFromJson] Sensor link id: {sensor.link_id}, Value: {json.get(sensor.link_id)}")
 
@@ -46,7 +46,7 @@ class Node:
         if sensor in self.sensorList:
             self.sensorList.remove(sensor)
 
-    def print_sensorList(self):
+    def print_sensor_list(self):
         for sensor in self.sensorList:
             print(f"-->{sensor.type}")
 
@@ -57,31 +57,29 @@ class Node:
         print(f"--> config_version = {self.config_version}")
         print(f"--> created_at = {self.created_at}")
         print(f"--> updated_at = {self.updated_at}")
-        if(not self.sensorList):
+        if not self.sensorList:
             print(f"--> sensorList = []")
         else:
             print(f"--> sensorList = [")
-            self.print_sensorList()
+            self.print_sensor_list()
             print(f"    ]")
 
-    def getDict(self):
-        temp_dict = {}
-        temp_dict['measure_time'] = datetime.now().strftime(
-            "%Y-%m-%d %H:%M:%S")
+    def get_dict(self):
+        temp_dict = {'measure_time': datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
         for sensor in self.sensorList:
-            temp_dict[sensor.link_id] = sensor.getDict()
+            temp_dict[sensor.link_id] = sensor.get_dict()
         return temp_dict
 
-    def getConfig(self):
+    def get_config(self):
         temp_list = []
         for sensor in self.sensorList:
-            temp_list.append(sensor.getConfig())
+            temp_list.append(sensor.get_config())
         return temp_list
 
-    @ classmethod  # create node object from json data
-    def from_JSON(cls, json):
+    @classmethod  # create node object from json data
+    def from_json(cls, json):
         node = Node.knownDevices.get(json['chipID'])
-        if(node is not None):
+        if node is not None:
             print(
                 f'[NODE] Node {json["chipID"]} is a\033[92m KNOWN\033[0m device')
             node.updated_at = datetime.now().strftime("%Y.%m.%d - %H:%M:%S")
@@ -93,12 +91,12 @@ class Node:
 
         return node
 
-    def saveNewDeviceToDb(self):
+    def save_new_device_to_db(self):
         """
             Saves a new device to the PWA database
         """
-        newDeviceApi = API(path='/new_device', params={"node_id": self.chipId})
-        response = newDeviceApi.post()
+        new_device_api = API(path='/new_device', params={"node_id": self.chipId})
+        response = new_device_api.post()
         if response is not None:
             print(f"[NEW DEVICE] {response.status_code}")
             if response.status_code == 200:
@@ -106,19 +104,18 @@ class Node:
         else:
             print(f"[NEW DEVICE] Network error: could not upload new device")
 
-    @ staticmethod  # initialise known devices from json
-    def knownDevices_from_JSON(json):
+    @staticmethod  # initialise known devices from json
+    def known_devices_from_json(json):
         for chipId in json:
             node = Node.knownDevices.get(int(chipId))
-            if (node is None):
+            if node is None:
                 # get sensor list
                 # print(json[chipId]['sensors'])
                 print(f"[Node] chipid = {chipId}, ", end='')
-                sensorList = Sensor.sensorsFromList(json[chipId]['sensors'])
+                sensor_list = Sensor.sensors_from_list(json[chipId]['sensors'])
 
                 # generate new node object
-                node = Node(int(chipId), 'unknown','',
-                            sensorList)  # FIXME version
+                node = Node(int(chipId), 'unknown', '', sensor_list)  # FIXME version
                 # print(f"Device with id {item['id']} is now known")
 
 # {
