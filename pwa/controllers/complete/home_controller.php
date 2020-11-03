7d446c343c850ae85dbbacb58194f6ea9758defb
Maarten <maarten.de.jong@hotmail.com>
2020-11-03 12:11:02 +0100

    Add weather forecast view to sensor data page
    and set sensor data page to home screen

--- pwa/controllers/complete/home_controller.php
+++ pwa/controllers/complete/home_controller.php
@@ -3,7 +3,11 @@ Class home_controller extends Controller{
 
 	public function __construct($arg){
 
-		$this->loadView('home/main');
+        $this->model = $this->loadModel('admin');
+
+        $this->loadView('data/content', array(
+            'sensor_data' => $this->model->getAdminPageSensorData()
+        ));
 
 	}
 
